cc4db5def6b3474633f6118c630f4fb3ff05eff2
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-06-19 17:39:21 -0300

    atualização da header

--- pipe/src/app/components/header/header.component.ts
+++ pipe/src/app/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AreaDeTrabalho } from 'src/app/models/area-de-trabalho.model';
+import { AreaDeTrabalhoService } from 'src/app/services/area-de-trabalho.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  areasDeTrabalho: AreaDeTrabalho[] = [];
 
+  constructor(private areaDeTrabalhoService: AreaDeTrabalhoService) {}
+
+  ngOnInit(): void {
+    this.carregarAreasDeTrabalho();
+  }
+
+  carregarAreasDeTrabalho(): void {
+    this.areaDeTrabalhoService.getAreasDeTrabalho().subscribe(
+      areas => {
+        this.areasDeTrabalho = areas;
+      },
+      error => {
+        console.error('Erro ao carregar áreas de trabalho:', error);
+      }
+    );
+  }
 }
